99337a22520b7c52846ad6efaffffed7493bd826
Marcos Isaias Garcia Martinez <xboxlive2846@gmail.com>
2026-03-03 18:41:09 -0600

    Initial Commit

---
+++ Aviation Crew/Aviation_CrewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aviation_CrewApp.swift
+//  Aviation Crew
+//
+//  Created by Marcos Garcia on 03/03/26.
+//
+
+import SwiftUI
+
+@main
+struct Aviation_CrewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
